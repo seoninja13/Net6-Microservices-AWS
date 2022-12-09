4f2e7998520882dabd1d287fe15909cb8468d823
seoninja13 <dachevivo@gmail.com>
2022-12-08 19:26:18 -0800

    Added Dependencies, simple route with return on Get.

--- Clodweather.Precipitation/Program.cs
+++ Clodweather.Precipitation/Program.cs
@@ -1,5 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+app.MapGet("/observation/{zipCode}", (string zipCode, [FromQuery] int? days) =>
+{
+    return Results.Ok("ZipCode is: " + zipCode);
+}); 
 
 app.Run();
